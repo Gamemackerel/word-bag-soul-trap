12c5c5ba7ed718e376ba6940cdc3913d905eda2d
Abraham Miller <abraham.r.m@gmail.com>
2026-04-09 11:42:59 -0700

    Build to dist/, fix index.html for dev

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ command }) => ({
-    base: command === 'build' ? './' : '/',
+    base: command === 'build' ? '/word-bag-soul-trap/dist/' : '/',
     build: {
-        outDir: '.',
-        emptyOutDir: false,
+        outDir: 'dist',
+        emptyOutDir: true,
     },
 }))
